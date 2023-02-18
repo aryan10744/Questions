855d316b25515562b3f1a2de6da5db723a668a20
Aryan Gupta <108828389+aryan10744@users.noreply.github.com>
2023-02-18 22:58:28 +0530

    Update temp3.cpp

--- temp3.cpp
+++ temp3.cpp
@@ -105,7 +105,8 @@ void solve(){
     int
     rep(i,1,n+1)cout<<l2[i]<<" ";
 }
-//miss misery
+//miss misery 
+//uploaded
 signed main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0);cout.tie(0);
